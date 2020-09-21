274351285c9063c6d6b9a5280b681f8416db7898
Isabelle Baert <webrelationpro@gmail.com>
2020-09-21 10:56:46 +0200

    create js toggle animation for collapse

---
+++ source/javascripts/toggleIcon.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $("#collapse1").on("hide.bs.collapse", function(){
+    $("#close-icon1").html('<img src="images/chevron_down_cn.png" alt="chevron down red" class="chevron-down">');
+  });
+  $("#collapse1").on("show.bs.collapse", function(){
+    $("#close-icon1").html('<img src="images/chevron_up_cn.png" alt="chevron up red" class="chevron-up">');
+  });
+  $("#collapse2").on("hide.bs.collapse", function(){
+    $("#close-icon2").html('<img src="images/chevron_down_cn.png" alt="chevron down red" class="chevron-down">');
+  });
+  $("#collapse2").on("show.bs.collapse", function(){
+    $("#close-icon2").html('<img src="images/chevron_up_cn.png" alt="chevron up red" class="chevron-up">');
+  });
+  $("#collapse3").on("hide.bs.collapse", function(){
+    $("#close-icon3").html('<img src="images/chevron_down_cn.png" alt="chevron down red" class="chevron-down">');
+  });
+  $("#collapse3").on("show.bs.collapse", function(){
+    $("#close-icon3").html('<img src="images/chevron_up_cn.png" alt="chevron up red" class="chevron-up">');
+  });
+  $("#collapse4").on("hide.bs.collapse", function(){
+    $("#close-icon4").html('<img src="images/chevron_down_cn.png" alt="chevron down red" class="chevron-down">');
+  });
+  $("#collapse4").on("show.bs.collapse", function(){
+    $("#close-icon4").html('<img src="images/chevron_up_cn.png" alt="chevron up red" class="chevron-up">');
+  });
+  $("#collapse5").on("hide.bs.collapse", function(){
+    $("#close-icon5").html('<img src="images/chevron_down_cn.png" alt="chevron down red" class="chevron-down">');
+  });
+  $("#collapse5").on("show.bs.collapse", function(){
+    $("#close-icon5").html('<img src="images/chevron_up_cn.png" alt="chevron up red" class="chevron-up">');
+  });
+});
