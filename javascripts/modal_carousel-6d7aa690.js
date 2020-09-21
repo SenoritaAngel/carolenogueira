efde16e5c82c4b5e5a595d948400201360ba0a59
Isabelle Baert <webrelationpro@gmail.com>
2020-09-21 09:52:08 +0200

    Automated commit at 2020-09-21 07:52:07 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/modal_carousel-6d7aa690.js
@@ -0,0 +1,10 @@
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "flex";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+;
